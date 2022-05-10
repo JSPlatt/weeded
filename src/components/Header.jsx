5f731e70dec5f9b116c35b0295e013ba59d3e491
Jeffrey Platt <jsplatt@gmail.com>
2022-05-10 14:50:17 -0400

    images added, logo added to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Logo from './img/logo.png'
 
 const Header = () => {
   return (
-    <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
+    <div className="fixed z-50 w-screen p-6 px-16">
         {/* desktop & tablet */}
         <div className="hidden md:flex w-full h-full">
-            
+            <div className="flex items-center gap-2">
+                <img src={Logo} className= "w-50 h-50 object-fill" alt="logo"/>
+                <p className="text-headingColor text-xl font-bold">City</p>
+            </div>
         </div>
         
         
