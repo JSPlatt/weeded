88472abcc6b6e5aabcf9dd44867eb9a7c3ff1698
Jeffrey Platt <jsplatt@gmail.com>
2022-05-10 14:05:19 -0400

    header started

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
+        {/* desktop & tablet */}
+        <div className="hidden md:flex w-full h-full">
+            
+        </div>
+        
+        
+        
+        {/* mobile */}
+        <div className="flex md:hidden w-full h-full">
+
+        </div>
+    </div>
   )
 }
 
