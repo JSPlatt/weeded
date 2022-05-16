c353378608c568d5e251263e55951ab67bb3277a
Jeffrey Platt <jsplatt@gmail.com>
2022-05-16 12:47:51 -0400

    avatar icon added, addtl containers created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,38 @@
 import React from 'react'
-import Logo from './img/logo.png'
+import Logo from '../img/logo.png'
+import Avatar from '../img/Avatar.png'
 import {MdShoppingBasket} from 'react-icons/md'
 
 const Header = () => {
   return (
     <header className="fixed z-50 w-screen p-6 px-16">
         {/* desktop & tablet */}
-        <div className="hidden md:flex w-full h-full">
+        <div className="hidden md:flex w-full h-full items-center justify-between">
             <div className="flex items-center gap-2">
-                <img src={Logo} className= "w-20 object-cover" alt="logo"/>
+                <img src={Logo} 
+                className= "w-20 object-cover rounded-full" 
+                alt="logo"/>
                 <p className="text-headingColor text-xl font-bold">City</p>
             </div>
 
-            <ul className="flex items-center gap-8 ml-auto">
-                <li className ="text-base text-textColor hoever:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Home</li>
-                <li className="text-base text-textColor hoever:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"> Menu</li>
-                <li className="text-base text-textColor hoever:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"> About Us</li>
-                <li className="text-base text-textColor hoever:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"> Service</li>
-            </ul>
-            <div className="relative flex items-center justify center">
-                <MdShoppingBasket className="text-textColor text-2xl ml-8 cursor-pointer"/>
-                <div className='absolute -top-2 -right-2 right-0 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
+            <div className="flex items-center gap-8">
+                <ul className="flex items-center gap-8 ">
+                    <li className ="text-base text-textColor hoever:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Home</li>
+                    <li className="text-base text-textColor hoever:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"> Menu</li>
+                    <li className="text-base text-textColor hoever:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"> About Us</li>
+                    <li className="text-base text-textColor hoever:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"> Service</li>
+                 </ul>
+                <div className="relative flex items-center justify center">
+                <MdShoppingBasket className="text-textColor text-2xl cursor-pointer"/>
+                    <div className='absolute -top-2 -right-2 right-0 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
                     <p className='text-xs text-white font-semibold'>2</p>
+                    </div>
                 </div>
+
+                <img 
+                    src={Avatar} 
+                    className='w-10 min-w-[40px] min-h-[40px] drop-shadow-xl rounded-full'
+                    alt="userprofile" />
             </div>
         </div>
         
