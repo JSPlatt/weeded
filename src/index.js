6e51eaccbdd7ecf0790de97586d03713a223a202
Jeffrey Platt <jsplatt@gmail.com>
2022-05-17 15:13:19 -0400

    updating to 18 not looking likely

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './index.css'
-
 import { BrowserRouter as Router } from 'react-router-dom'
-
 import App from './App'
 
 ReactDOM.render(
