31f4bb1cc665cad0f8686e0949494daaba9da179
Jeffrey Platt <jsplatt@gmail.com>
2022-05-23 12:51:05 -0400

    some style addts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../img/logo.png'
 import Avatar from '../img/Avatar.png'
 import {MdShoppingBasket, MdAdd, MdLogout} from 'react-icons/md'
@@ -15,6 +15,8 @@ const Header = () => {
     const provider = new GoogleAuthProvider()
     const [{user}, dispatch] = useStateValue()
 
+    const [isMenu, setIsMenu] = useState(false)
+
     const login = async () => {
         if (!user){
                 const {
@@ -24,6 +26,8 @@ const Header = () => {
                 user : providerData[0],
             })
             localStorage.setItem("user", JSON.stringify(providerData[0]))
+        }else{
+            setIsMenu(!isMenu)
         }
     }
 
@@ -59,7 +63,13 @@ const Header = () => {
                         alt="userprofile" 
                         onClick={login}
                 />
-                <div className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
+                {
+                    isMenu && (
+                    <motion.div 
+                        initial={{ opacity : 0, scale : 0.6 }} 
+                        animate={{ opacity : 1, scale : 1 }}
+                        exit={{ opacity : 0, scale : 0.6 }}
+                        className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
                     {
                         user && user.email === "jsplatt@gmail.com" && (
                         <Link to={"/createitem"}>
@@ -70,7 +80,9 @@ const Header = () => {
 
 
                     <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">Logout <MdLogout /></p>
-                </div>
+                    </motion.div>
+                    )
+                }
                 
                 </div>
             </div>
