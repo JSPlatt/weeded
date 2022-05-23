6777058a2996a7c2b039e35b61a9718b79818895
Jeffrey Platt <jsplatt@gmail.com>
2022-05-23 12:39:18 -0400

    item link added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Logo from '../img/logo.png'
 import Avatar from '../img/Avatar.png'
-import {MdShoppingBasket} from 'react-icons/md'
+import {MdShoppingBasket, MdAdd, MdLogout} from 'react-icons/md'
 import { motion } from 'framer-motion'
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from '../firebase.config'
@@ -59,9 +59,17 @@ const Header = () => {
                         alt="userprofile" 
                         onClick={login}
                 />
-                <div className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0 px-4 py-2">
-                    <p>New Item</p>
-                    <p>Logout</p>
+                <div className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
+                    {
+                        user && user.email === "jsplatt@gmail.com" && (
+                        <Link to={"/createitem"}>
+                            <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">New Item <MdAdd />
+                            </p>
+                        </Link>
+                    )}
+
+
+                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">Logout <MdLogout /></p>
                 </div>
                 
                 </div>
