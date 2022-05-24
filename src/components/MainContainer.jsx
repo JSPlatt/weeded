94833973242fd4401487082ba43b8ea0a6f29b90
Jeffrey Platt <jsplatt@gmail.com>
2022-05-24 11:34:09 -0400

    del icon pulled, disp, working on style

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -5,9 +5,11 @@ const MainContainer = () => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 gap-2'>
       <div className="py-2 flex-1 flex flex-col items-start md:items-center justify-center">
-      <div>
-        <MdDeliveryDining/>
+      <div className='flex items-center gap-2 justify-center'>
         <p>Delivery</p>
+        <div className='w-10 h-10 bg-white rounded-full overflow-hidden'>
+          <MdDeliveryDining className='w-full h-full object-contain'/>
+        </div>
       </div>
         
       </div>
