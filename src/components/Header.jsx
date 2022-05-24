0208fa76ebbf0133e81ba2fa857b997089136ae7
Jeffrey Platt <jsplatt@gmail.com>
2022-05-24 10:45:44 -0400

    mobile responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -63,6 +63,7 @@ const Header = () => {
                         <li className="text-base text-textColor hoever:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"> About Us</li>
                         <li className="text-base text-textColor hoever:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"> Service</li>
                 </motion.ul>
+
                 <div className="relative flex items-center justify center">
                 <MdShoppingBasket className="text-textColor text-2xl cursor-pointer"/>
                     <div className='absolute -top-2 -right-2 right-0 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
@@ -112,6 +113,14 @@ const Header = () => {
                 alt="logo"/>
                 <p className="text-headingColor text-xl font-bold">City</p>
         </Link>
+
+        <div className="relative flex items-center justify center">
+                <MdShoppingBasket className="text-textColor text-2xl cursor-pointer"/>
+                    <div className='absolute -top-2 -right-2 right-0 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
+                    <p className='text-xs text-white font-semibold'>2</p>
+                    </div>
+                </div>
+
             <div className="relative">
                     <motion.img 
                         whileTap={{scale: 0.6}}
