7548bc1cc1b13410086ec721de88a27ac61bb69d
Jeffrey Platt <jsplatt@gmail.com>
2022-05-25 15:51:16 -0400

    fixed CSS errors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -58,15 +58,15 @@ const Header = () => {
                     animate= {{ opacity : 1, x : 0 }}
                     exit= {{ opacity: 0, x : 200 }}
                     className="flex items-center gap-8 ">
-                        <li className ="text-base text-textColor hoever:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Home</li>
-                        <li className="text-base text-textColor hoever:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"> Menu</li>
-                        <li className="text-base text-textColor hoever:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"> About Us</li>
-                        <li className="text-base text-textColor hoever:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"> Service</li>
+                        <li className ="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Home</li>
+                        <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"> Menu</li>
+                        <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"> About Us</li>
+                        <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"> Service</li>
                 </motion.ul>
 
                 <div className="relative flex items-center justify center">
                 <MdShoppingBasket className="text-textColor text-2xl cursor-pointer"/>
-                    <div className='absolute -top-2 -right-2 right-0 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
+                    <div className='absolute -top-2 right-0 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
                     <p className='text-xs text-white font-semibold'>2</p>
                     </div>
                 </div>
@@ -109,7 +109,7 @@ const Header = () => {
         <div className="flex items-center justify-between md:hidden w-full h-full">
             <div className="relative flex items-center justify center">
                 <MdShoppingBasket className="text-textColor text-2xl cursor-pointer"/>
-                <div className='absolute -top-2 -right-2 right-0 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
+                <div className='absolute -top-2 right-0 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
                     <p className='text-xs text-white font-semibold'>2</p>
                 </div>
             </div>
