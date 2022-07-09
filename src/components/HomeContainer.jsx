f2785ea82aeea748372f2a37254250a1fa796a21
Jeffrey Platt <jsplatt@gmail.com>
2022-07-08 20:51:22 -0400

    styling fixed

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -35,11 +35,13 @@ const HomeContainer = () => {
         className='ml-auto h-420 w-full lg:w-auto lg:h-650' alt="Hero-Background" 
         />
        
-        <div className='w-full h-full absolute top-0 left-0 flex items-center justify-center px-32 py-4'>
+        <div className='w-full h-full absolute top-0 left-0 flex items-center justify-center px-32 py-4 gap-4 flex-wrap'>
             {heroData && heroData.map(n => (
-                <div key={n.id}className='w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center'>
+                <div 
+                key={n.id} 
+                className='w-190 min-w-[190px] p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center'>
                     <img src={n.imageSrc} 
-                        className="w-40 -mt-20"
+                        className="w-40-mt-20 rounded-3xl"
                         alt="cannabis flower"
                      />
                         <p className='text-xl font-semibold text-textColor mt-3'>
