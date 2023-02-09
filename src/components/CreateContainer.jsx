3589bbcc4e333a7799553d42aa9b522813c7fc57
Jeffrey Platt <jsplatt@gmail.com>
2023-02-09 13:31:11 -0500

    alert field done

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 
 const CreateContainer = () => {
+  
   const [title, setTitle] = useState("")
   const [type, setType] = useState("")
   const [price, setPrice] = useState("")
@@ -9,11 +10,19 @@ const CreateContainer = () => {
   const [fields, setFields ] = useState(false)
   const [alertStatus, setAlertStatus] = useState('danger')
   const [msg, setMsg] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const [imageAsset, setImageAsset] = useState(null)
 
   return (
     <div className='w-full min-h-screen p-2 flex items-center justify-center'>
       <div className='w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center'>
-
+        {
+          fields && (
+            <p className={`w-full p-2 rounded-lg text-center text-lg font-semibold ${alertStatus === 'danger'? 'bg-red-400 text-red-800' : 'bg-emerald-400 text-emerald-800'}`}>
+              {msg}
+            </p>
+          )
+        }
       </div>
     </div>
   )
