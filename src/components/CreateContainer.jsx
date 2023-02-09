7e6978033bfa0f0713db3080feca962ac225d25a
Jeffrey Platt <jsplatt@gmail.com>
2023-02-09 13:15:57 -0500

    state started

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
 
 const CreateContainer = () => {
+  const [title, setTitle] = useState("")
+  const [type, setType] = useState("")
+  const [price, setPrice] = useState("")
+  const [category, setCategory] = useState(null)
+  const [fields, setFields ] = useState(false)
+  const [alertStatus, setAlertStatus] = useState('danger')
+  const [msg, setMsg] = useState(null)
+
   return (
-    <div className='w-full h-auto p-2'>Create Container</div>
+    <div className='w-full min-h-screen p-2 flex items-center justify-center'>
+      <div className='w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center'>
+
+      </div>
+    </div>
   )
 }
 
